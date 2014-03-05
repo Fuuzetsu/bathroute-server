7a845ab62287ee9978e485eba0bcf14138fa9938
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-05 00:36:00 +0000

    Project stub

---
+++ README.md
@@ -0,0 +1,5 @@
+Bathroute-server
+----------------
+
+Server program for the
+[Bathroute](http://github.com/Fuuzetsu/bathroute) Android application.
---
+++ src/Main.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE UnicodeSyntax #-}
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) Mateusz Kowalczyk 2014
+-- License     :  GPL-3
+--
+-- Maintainer  :  fuuzetsu@fuuzetsu.co.uk
+-- Stability   :  experimental
+--
+-- Entry point for the Bathroute server.
+module Main where
+
+main ∷ IO ()
+main = return ()
