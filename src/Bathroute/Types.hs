249c558df078098d71b82e94b217fde958ab73cd
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-07 06:51:31 +0000

    Better request structure

--- src/Bathroute/Types.hs
+++ src/Bathroute/Types.hs
@@ -10,6 +10,9 @@
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 -- |
 -- Module      :  Bathroute.Types
@@ -24,9 +27,12 @@ module Bathroute.Types where
 
 import           Control.Applicative
 import           Codec.Crypto.RSA.Pure
+import           Control.Monad
 import           Control.Monad.IO.Class (liftIO)
 import           Data.Aeson
 import           Data.Aeson.TH
+import           Data.Binary (get, put, Binary, putWord8, getWord8)
+import qualified Data.Binary as B
 import           Data.ByteString
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8 as C8
@@ -39,17 +45,19 @@ import qualified Data.Conduit.List as CL
 import           Data.Conduit.Network
 import qualified Data.Conduit.Text as CT
 import           Data.Data
-import           Data.Foldable
+import           Data.DeriveTH
+import           Data.Foldable hiding (mapM_)
 import           Data.Hashable
 import           Data.Text
 import qualified Data.Text.Encoding as TE
 import           Data.Traversable
 import           Data.Typeable
 
+$(deriveJSON defaultOptions ''PublicKey)
 
 -- | The standard ByteString instances in aeson are broken: they assume UTF8
 -- encoding. This type instead uses base64 for proper round-tripping.
-newtype ByteString64 = ByteString64 { unByteString64 :: ByteString }
+newtype ByteString64 = ByteString64 { unByteString64 ∷ ByteString }
                      deriving (Eq, Read, Show, Data,
                                Typeable, Ord, Hashable)
 
@@ -86,19 +94,100 @@ instance FromJSON ByteString64 where
   parseJSON o =
     parseJSON o >>= either fail (return . ByteString64) . B64.decode
 
--- | A 'Request' is a pair of
-data Request a = Request PublicKey a
-               deriving (Functor, Foldable, Traversable, Eq, Show)
+-- | Actions one can request with a person.
+data FriendAction = Add PublicKey | Remove PublicKey
+                  | Block PublicKey | Share PublicKey
+                  deriving (Eq, Show)
 
--- | When a user requests a new alias, we decode it to this data type which
--- we later verify the signature of.
-data AliasRequest =
-  AliasRequest { aliasName ∷ String
-                 -- ^ The new name that the user wishes to use.
-               }
-  deriving (Eq, Show)
+-- $(deriveJSON defaultOptions ''FriendAction)
 
-data OnlineRequest = OnlineRequest
+-- -- | When a user requests a new alias, we decode it to this data type which
+-- -- we later verify the signature of.
+-- data AliasRequest =
+--   AliasRequest { aliasName ∷ String
+--                  -- ^ The new name that the user wishes to use.
+--                }
+--   deriving (Eq, Show)
+
+-- $(deriveJSON defaultOptions ''AliasRequest)
+
+-- | User status change.
+data OnlineRequest = Online | Offline
                    deriving (Eq, Show)
 
-$(deriveJSON defaultOptions ''PublicKey)
+$(deriveJSON defaultOptions ''OnlineRequest)
+
+-- -- | Key of a person user wishes to interact with and an action describing
+-- -- what they want to do.
+-- data FriendRequest = FriendRequest { friendKey ∷ PublicKey
+--                                    , friendAction ∷ FriendAction }
+
+-- $(deriveJSON defaultOptions ''FriendRequest)
+-- $(deriveJSON defaultOptions ''Request)
+
+-- verify' ∷ Request ByteString
+--         → Either (Either String RSAError) (Maybe ByteString64)
+-- verify' req@(Request k r s m) = case B64.decode m of
+--   Left e → Left $ Left e
+--   Right s' → case verify k (LB.fromStrict m) (LB.fromStrict s') of
+--     Left rsa → Left $ Right rsa
+--     Right False → Right Nothing
+--     Right True → Right $ Just m
+
+
+data ServerComm = ServerComm { senderKey ∷ PublicKey
+                             , senderSignature ∷ ByteString
+                               -- ^ Signed message to the server itself
+                               -- protecting recipient.
+                             , serverMessage ∷ ByteString
+                             }
+
+data ServerMessage = ServerMessage { requestType ∷ RequestType
+                                   , secretMessage ∷ ByteString
+                                   , messageSignature ∷ ByteString
+                                     -- ^ Signed contents of the message
+                                   , messageRecipient ∷ PublicKey
+                                   }
+
+data RequestType = OnlineChange | FriendChange
+
+$(derive makeBinary ''RequestType)
+$(derive makeBinary ''OnlineRequest)
+$(derive makeBinary ''FriendAction)
+
+instance Binary ServerComm where
+  put (ServerComm k s m) = put (k, s, m)
+  get = liftM3 ServerComm get get get
+
+instance Binary ServerMessage where
+  put (ServerMessage t m s r) = put (t, m, s, r)
+  get = liftM4 ServerMessage get get get get
+
+type ServerKeys = (PublicKey, PrivateKey)
+
+data Request = OnlineStatus OnlineRequest | FriendStatus FriendAction
+
+-- | Main decoder which takes care of decrypting and deserialising messages
+decodeMessage ∷ ServerKeys → ServerComm → Either String (PublicKey, Request)
+decodeMessage (spub, spriv) (ServerComm sendpub sendsig servmsg) =
+  -- First verify that thet the message server is getting is what's intended
+  case verify sendpub (LB.fromStrict servmsg) (LB.fromStrict sendsig) of
+    Left rsaerr → Left $ show rsaerr -- failed to decode properly
+    Right False → Left "Failed to verify signature on serverMessage"
+    Right True → case decrypt spriv (LB.fromStrict servmsg) of
+      Left rsaerr → Left $ show rsaerr
+      Right xs →
+        let ServerMessage t ms sig r = B.decode xs
+        in case verify sendpub (LB.fromStrict sig) (LB.fromStrict ms) of
+          Left rsaerr → Left $ show rsaerr
+          Right False → Left "Failed to verify signature on secretMessage"
+          Right True
+            | spub == r → case dec ms of
+              Left rsaerr → Left $ show rsaerr
+              Right bs → fmap (sendpub,) $ case t of
+                OnlineChange → Right . OnlineStatus $ B.decode bs
+                FriendChange → Right . FriendStatus $ B.decode bs
+            | otherwise → Left "No handling of messages to others yet"
+
+            where
+              dec = decrypt spriv . LB.fromStrict
