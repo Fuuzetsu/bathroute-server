b96fcca764e8ba5c77c62752223f58c4c9e250e9
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-04-05 18:27:42 +0100

    Clean out crypto + DB stuff don't need right now

--- src/Bathroute/Communication.hs
+++ src/Bathroute/Communication.hs
@@ -28,105 +28,34 @@
 -- Handles communication with the clients.
 module Communication where
 
-import Bathroute.Types
-import           Codec.Crypto.RSA.Pure
-import           Control.Applicative
-import           Control.Monad
+import           Bathroute.Types
 import           Control.Monad.IO.Class (liftIO)
 import           Data.Aeson
-import           Data.Aeson.TH
-import           Data.Binary (get, put, Binary, putWord8, getWord8)
-import qualified Data.Binary as B
-import           Data.ByteString
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Base64 as B64
-import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString.Lazy as LB
-import qualified Data.ByteString.UTF8 as U8
 import           Data.Conduit
-import qualified Data.Conduit.Binary as CB
-import qualified Data.Conduit.List as CL
-import           Data.Conduit.Network
-import qualified Data.Conduit.Text as CT
-import           Data.Data
-import           Data.DeriveTH
-import           Data.Foldable hiding (mapM_)
-import           Data.Hashable
-import Data.List ((\\))
-import           Data.Text hiding (words, unwords)
-import qualified Data.Text.Encoding as TE
-import           Data.Traversable
-import           Data.Typeable
-import qualified Database.Esqueleto as E
-import Database.Persist hiding (Add)
-import Database.Persist.Quasi
-import qualified Database.Persist.Sqlite as S
-import Database.Persist.TH
 
-handler ∷ ServerKeys
-        → Conduit ByteString IO Status
-handler sk@(pub,priv) = awaitForever $ \msg → do
-  case decodeStrict msg of
-    Nothing → yield $ Failed "Can't decode message from client"
-    Just sc → case decodeMessage sk sc of
-      Left e → yield $ Failed e
-      Right (pk, r) → yield =<< case r of
-        OnlineStatus x → liftIO (updateOnline pk x)
-        FriendStatus x → liftIO (insertFriend pk x)
-        AliasStatus x → liftIO (insertAlias pk x)
+handler ∷ Conduit BS.ByteString IO Status
+handler = awaitForever $ \msg →
+  case decode $ LB.fromStrict msg of
+    Nothing → yield $ Failed "Failed to decode JSON in handler"
+    Just (ServerMessage { recipient = pk, request = x }) → yield =<< case x of
+      OnlineStatus x' → liftIO (updateOnline pk x')
+      FriendStatus x' → liftIO (insertFriend pk x')
+      AliasStatus x' → liftIO (insertAlias pk x')
 
-updateOnline ∷ PublicKey → OnlineRequest → IO Status
-updateOnline pk s =
+updateOnline ∷ Integer → OnlineRequest → IO Status
+updateOnline _pk _s =
   return $ Failed "updateOnline not implemented"
 
 -- | Fails when alias exists.
-insertAlias ∷ PublicKey → AliasRequest → IO Status
-insertAlias pk (DesiredAlias s) = S.runSqlite ":memory:" $ do
-  als ← E.select . E.from $ \al → E.where_ (al E.^. AliasName E.==. E.val s)
-  case als of
-    [] → insert (Alias s $ keyToBS pk) >> return OK
-    _ → return . Failed $ unwords ["Alias", s, "already exists"]
+insertAlias ∷ Integer → AliasRequest → IO Status
+insertAlias _pk (DesiredAlias _s) =
+  return $ Failed "insertAlias not implemented"
 
-keyToBS ∷ PublicKey → ByteString
-keyToBS = LB.toStrict . B.encode
-
-insertFriend ∷ PublicKey → FriendAction → IO Status
-insertFriend pk fa = S.runSqlite ":memory:" $ do
-  case fa of
-    Add k → return $ Failed "Adding not implemented"
-    Remove k → return $ Failed "Removing not implemented"
---       users ← E.update $ \u → do
---         let f (xs :: E.SqlExpr (E.Value [UserId])) = xs -- fmap fmap fmap (\\ [keyToBS k]) xs
---         E.set u [ FriendsFriendsWith E.=. (f (u E.^. FriendsFriendsWith)) ]
-
--- --        return (u E.^. UserPublicKey)
-
---       liftIO $ Prelude.putStrLn "hello"
---      E.update $ \u → E.set u [FriendsFriendsWith E.=. E.val []]
-    Block k → return $ Failed "Blocking not implemented"
-    Share k → return $ Failed "Sharing not implemented"
-
--- | Main decoder which takes care of decrypting and deserialising messages
-decodeMessage ∷ ServerKeys → ServerComm → Either String (PublicKey, Request)
-decodeMessage (spub, spriv) (ServerComm sendpub sendsig servmsg) =
-  -- First verify that thet the message server is getting is what's intended
-  case verify sendpub (LB.fromStrict servmsg) (LB.fromStrict sendsig) of
-    Left rsaerr → Left $ show rsaerr -- failed to decode properly
-    Right False → Left "Failed to verify signature on serverMessage"
-    Right True → case decrypt spriv (LB.fromStrict servmsg) of
-      Left rsaerr → Left $ show rsaerr
-      Right xs →
-        let ServerMessage t ms sig r = B.decode xs
-        in case verify sendpub (LB.fromStrict sig) (LB.fromStrict ms) of
-          Left rsaerr → Left $ show rsaerr
-          Right False → Left "Failed to verify signature on secretMessage"
-          Right True
-            | spub == r → case dec ms of
-              Left rsaerr → Left $ show rsaerr
-              Right bs → fmap (sendpub,) $ case t of
-                OnlineChange → Right . OnlineStatus $ B.decode bs
-                FriendChange → Right . FriendStatus $ B.decode bs
-            | otherwise → Left "No handling of messages to others yet"
-
-            where
-              dec = decrypt spriv . LB.fromStrict
+insertFriend ∷ Integer → FriendAction → IO Status
+insertFriend _pk fa = case fa of
+  Add _k → return $ Failed "Adding not implemented"
+  Remove _k → return $ Failed "Removing not implemented"
+  Block _k → return $ Failed "Blocking not implemented"
+  Share _k → return $ Failed "Sharing not implemented"
--- src/Bathroute/Types.hs
+++ src/Bathroute/Types.hs
@@ -28,106 +28,26 @@
 -- Types used throughout the server.
 module Bathroute.Types where
 
-import           Control.Applicative
-import           Codec.Crypto.RSA.Pure
-import           Control.Monad
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson
+import           Control.Concurrent
+import           Control.Concurrent.STM
 import           Data.Aeson.TH
 import           Data.Binary (get, put, Binary, putWord8, getWord8)
-import qualified Data.Binary as B
-import           Data.ByteString
-import qualified Data.ByteString.Base64 as B64
-import qualified Data.ByteString.Char8 as C8
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.UTF8 as U8
-import qualified Data.ByteString.Lazy as LB
-import           Data.Conduit
-import qualified Data.Conduit.Binary as CB
-import qualified Data.Conduit.List as CL
-import           Data.Conduit.Network
-import qualified Data.Conduit.Text as CT
-import           Data.Data
 import           Data.DeriveTH
-import           Data.Foldable hiding (mapM_)
-import           Data.Hashable
+import           Data.Map
 import           Data.Text
-import qualified Data.Text.Encoding as TE
-import           Data.Traversable
-import           Data.Typeable
--- import Database.Persist
-import Database.Persist.TH
-import qualified Database.Persist.Sqlite as S
-import Database.Persist.Quasi
 
-$(deriveJSON defaultOptions ''PublicKey)
+data User = User Integer
 
--- | The standard ByteString instances in aeson are broken: they assume UTF8
--- encoding. This type instead uses base64 for proper round-tripping.
-newtype ByteString64 = ByteString64 { unByteString64 ∷ ByteString }
-                     deriving (Eq, Read, Show, Data,
-                               Typeable, Ord, Hashable)
+type Latitude = Double
+type Longitude = Double
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-  User
-    publicKey ByteString
-    deriving Eq Show
-  Event
-    latitude Double
-    longitude Double
-    description String
-    creatorid UserId
-    deriving Eq Show
-  Alias
-    name String
-    ownerKey ByteString
-    deriving Eq Show
-  PendingFriends
-    adder UserId
-    addee UserId
-    deriving Eq Show
-  Friends
-    listOwner UserId
-    friendsWith [UserId]
-    deriving Eq Show
-|]
-
--- | Lazy ByteString to Text
-strict :: LB.ByteString -> Text
-strict = TE.decodeUtf8 . BS.concat . LB.toChunks
-{-# INLINE strict #-}
-
--- | Text to lazy ByteString
-lazy :: Text -> LB.ByteString
-lazy = LB.fromChunks . (:[]) . TE.encodeUtf8
-{-# INLINE lazy #-}
-
-instance ToJSON BS.ByteString where
-    toJSON = String . TE.decodeUtf8
-    {-# INLINE toJSON #-}
-
-instance FromJSON BS.ByteString where
-    parseJSON = withText "ByteString" $ pure . TE.encodeUtf8
-    {-# INLINE parseJSON #-}
-
-instance ToJSON LB.ByteString where
-    toJSON = toJSON . strict
-    {-# INLINE toJSON #-}
-
-instance FromJSON LB.ByteString where
-    parseJSON = withText "Lazy ByteString" $ pure . lazy
-    {-# INLINE parseJSON #-}
-
-instance ToJSON ByteString64 where
-  toJSON (ByteString64 bps) = toJSON (B64.encode bps)
-
-instance FromJSON ByteString64 where
-  parseJSON o =
-    parseJSON o >>= either fail (return . ByteString64) . B64.decode
+data Event = Event Latitude Longitude User
+data Alias = Alias String User
+data Friends = Friends (Map User [User])
 
 -- | Actions one can request with a person.
-data FriendAction = Add PublicKey | Remove PublicKey
-                  | Block PublicKey | Share PublicKey
+data FriendAction = Add Integer | Remove Integer
+                  | Block Integer | Share Integer
                   deriving (Eq, Show)
 
 -- | User status change.
@@ -144,40 +64,27 @@ data Request = OnlineStatus OnlineRequest
              | FriendStatus FriendAction
              | AliasStatus AliasRequest
 
-
-data ServerComm = ServerComm { senderKey ∷ PublicKey
-                             , senderSignature ∷ ByteString
-                               -- ^ Signed message to the server itself
-                               -- protecting recipient.
-                             , serverMessage ∷ ByteString
-                             }
-
 data RequestType = OnlineChange | FriendChange
 
-data ServerMessage = ServerMessage { requestType ∷ RequestType
-                                   , secretMessage ∷ ByteString
-                                   , messageSignature ∷ ByteString
-                                     -- ^ Signed contents of the message
-                                   , messageRecipient ∷ PublicKey
+data ServerMessage = ServerMessage { recipient ∷ Integer
+                                   , request ∷ Request
                                    }
+
+
+
+newtype Server = Server {
+  _clients ∷ MVar (Map ThreadId (Text → STM ()))
+  }
 
 $(derive makeBinary ''RequestType)
 $(derive makeBinary ''AliasRequest)
 $(derive makeBinary ''Request)
 $(derive makeBinary ''OnlineRequest)
 $(derive makeBinary ''FriendAction)
+$(deriveJSON defaultOptions ''FriendAction)
+$(deriveJSON defaultOptions ''Request)
+$(deriveJSON defaultOptions ''ServerMessage)
 
-instance Binary ServerComm where
-  put (ServerComm k s m) = put (k, s, m)
-  get = liftM3 ServerComm get get get
-
-instance Binary ServerMessage where
-  put (ServerMessage t m s r) = put (t, m, s, r)
-  get = liftM4 ServerMessage get get get get
-
-$(deriveJSON defaultOptions ''ServerComm)
-
-type ServerKeys = (PublicKey, PrivateKey)
 data Status = OK | Failed String deriving (Show, Eq)
 
 $(derive makeBinary ''Status)
